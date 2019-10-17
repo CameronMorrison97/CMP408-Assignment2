3880bb534d30b141d253d6347b8bd8cde01cddc9
CameronMorrison97 <cameron.morrison1997@gmail.com>
2019-10-17 10:06:04 +0100

    Stuff

--- webcontrol.php
+++ webcontrol.php
@@ -3,27 +3,35 @@
 		<meta name="author" content="Cameron Morrison">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		
-		<?php 
-			if(isset($_POST['pin']) == true && isset($_POST['value']) == true){
-				echo $_POST['pin'];
-			}
-		?>
-		
 		<!-- 
 			Importing Scripts
 		!-->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-		<script src="ajax.js"></script>
+		<script type="text/javascript">
+			function sendData(){
+				console.log("Stuff");
+				$(document).ready(function(){
+					var pinjs = $("#pin").val();
+					var valuejs = $("#value").val();
+					
+					$.ajax({url: "ajax.php",type: "POST", data: {pinNum: pinjs}, success: function(data){
+							if(data.includes("set")){
+								$("form").append("<div class='alert alert-success' role='alert'>Success</div>");
+							}
+					}});
+				});
+			}
+		</script>
 		
 		<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
 	</head>
 	<body>
 		<div class="form form-group">
-			<form action="webcontrol.php" method="post">
-				GPIO Pin Number : <input type="text" name="pin" onkeyup="sendData()"></input></br>
-				GPIO Pin Value : <input type="text" name="value"></input></br>
-				<input type="button" onclick="" class="btn btn-primary"></input>
+			<form>
+				GPIO Pin Number : <input id="pin" type="text" name="pinNum"></input></br>
+				GPIO Pin Value : <input id="value" type="text"></input></br>
+				<input type="button" value="Submit" onclick="sendData();" class="btn btn-primary"></input>
 			</form>
 		</div>
 	</body>
