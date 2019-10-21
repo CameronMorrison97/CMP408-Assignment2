983f9f05bf3cda20753532c13499e328fd07f761
abertay <abertay@localhost.localdomain>
2019-10-21 18:02:52 +0100

    Light working xD

--- ajax.php
+++ ajax.php
@@ -5,7 +5,7 @@
 			if(($_POST["pinNum"] >= 0 && $_POST["pinNum"] <= 41) && ($_POST["valueNum"] == 0 || $_POST["valueNum"] == 1)){
 				// Don't judge me bro :DDD
 				// I know it's not secure will maybe sanitise later I guess XD
-				echo shell_exec("sudo ./piiotest write " . $_POST["pinNum"] . " " . $_POST["valueNum"] . " 2> error.txt");
+				echo shell_exec("sudo /var/www/html/CMP408-Assignment2/piiotest write " . $_POST["pinNum"] . " " . $_POST["valueNum"]);
 				echo "set";	
 			}
 	}else{
