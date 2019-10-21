a800bfe0a4d355d4fa7161d4d4ed255ed45c6f09
abertay <abertay@localhost.localdomain>
2019-10-21 17:17:20 +0100

    Testing Web validation

--- ajax.php
+++ ajax.php
@@ -1,11 +1,9 @@
 <?php 
+	$valid = false;
+
 	if(is_numeric($_POST["pinNum"]) == true && is_numeric($_POST["valueNum"]) == true){
-			if($_POST["pinNum"] >= 0 && $_POST["pinNum"] <= 41){
-				echo "Valid pinNum";
-			}
-			
-			if($_POST["valueNum"] == 0 || $_POST["valueNum"] == 1){
-				echo "Valid Value";
+			if(($_POST["pinNum"] >= 0 && $_POST["pinNum"] <= 41) && ($_POST["valueNum"] == 0 || $_POST["valueNum"] == 1)){
+				echo "set";	
 			}
 	}else{
 		echo "Invalid";
