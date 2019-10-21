415cee68a5f1546d05f17bd133a61a0e6dd6473c
abertay <abertay@localhost.localdomain>
2019-10-21 17:39:22 +0100

    Shell exec

--- ajax.php
+++ ajax.php
@@ -3,6 +3,7 @@
 
 	if(is_numeric($_POST["pinNum"]) == true && is_numeric($_POST["valueNum"]) == true){
 			if(($_POST["pinNum"] >= 0 && $_POST["pinNum"] <= 41) && ($_POST["valueNum"] == 0 || $_POST["valueNum"] == 1)){
+				shell_exec("sudo ./piiotest write " . $_POST["pinNum"] . " " . $_POST["valueNum"]);
 				echo "set";	
 			}
 	}else{
